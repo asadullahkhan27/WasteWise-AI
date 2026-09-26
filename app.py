838ccbb3506ec5e1d6d6fa2fe65c7aa97d87ca9c
Asad Ullah Khan <asadullahkhan231569@gmail.com>
2026-09-26 11:38:12 +0500

    Update logo image display parameters
    Removed height parameter from image display for logos.

--- app.py
+++ app.py
@@ -1098,8 +1098,7 @@ with logo_col2:
 
         st.image(
             str(ICODEGURU_LOGO),
-            width=65,
-            height=100
+            width=65
         )
 
     else:
@@ -1115,8 +1114,7 @@ with logo_col3:
 
         st.image(
             str(SCHOOL_LOGO),
-            width=65,
-            height=100
+            width=65
         )
 
     else:
